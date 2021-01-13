ddde2b86e29d46aa63e88cd20f6c583f8bda93e9
VasiliKnyazev <basil_jr_king@mail.ru>
2021-01-13 10:04:02 +0600

    Added sleuth, zipkin and rabbitmq support

--- src/main/java/com/luxoft/education/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/luxoft/education/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.luxoft.education.microservices.currencyexchangeservice.controller;
 
 import com.luxoft.education.microservices.currencyexchangeservice.model.ExchangeValue;
 import com.luxoft.education.microservices.currencyexchangeservice.service.ExchangeValueService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     private Environment environment;
     private ExchangeValueService exchangeValueService;
 
@@ -24,6 +27,7 @@ public class CurrencyExchangeController {
         System.out.println("from: " + from + " to: " + to);
         ExchangeValue exchangeValue = exchangeValueService.findByFromAndTo(from , to);
         exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+        logger.info("LOGGER: exchangeValue: {}", exchangeValue);
         return exchangeValue;
     }
 
